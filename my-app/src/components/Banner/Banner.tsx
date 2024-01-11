d1ff8475e2250d130c11c96cb5cf7eed8f73603c
piotr kaczmarek <89982700+piotrkaczmarek123@users.noreply.github.com>
2024-01-11 15:33:54 +0100

    dodanie apki do rozwoju

---
+++ my-app/src/components/Banner/Banner.tsx
@@ -0,0 +1,11 @@
+// /src/components/Banner/Banner.tsx
+import React from 'react';
+import './Banner.css';
+
+const Banner: React.FC = () => (
+  <div id="banner">
+    <h1>Hello, welcome to your project</h1>
+  </div>
+);
+
+export default Banner;
