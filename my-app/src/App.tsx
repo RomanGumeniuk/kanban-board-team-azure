00d120bd37e9e2699c5c6cd30ad680fa2e6f2d4c
JakubWisla-WTT <jakub.wisla1@wundermanthompson.com>
2024-01-11 13:43:51 +0100

    1

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Notes from './Notes.js'
 import './App.css'
 
 function App() {
+  <div>
+  <Notes />
+</div>
   const [count, setCount] = useState(0)
 
   return (
---
+++ my-app/src/Notes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Notes() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+
+export default MyComponent;
