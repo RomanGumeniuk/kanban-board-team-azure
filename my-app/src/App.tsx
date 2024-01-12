b32e8739c7f21eb8ea22eee66afb388d07425351
Romanek <89982697+RomanGumeniuk@users.noreply.github.com>
2024-01-12 10:21:31 +0100

    Added Columns horizontal

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,12 +5,13 @@ import Banner from './components/Banner/Banner';
 import NoteContainer from './components/NoteContainer/NoteContainer';
 
 const App: React.FC = () => (
-  <div id='main-div'>
+  <div id="main-div">
     <Banner />
     <NoteContainer title="To Do" />
     <NoteContainer title="In Progress" />
     <NoteContainer title="For Review"/>
     <NoteContainer title="Done" />
+    
   </div>
 );
 
