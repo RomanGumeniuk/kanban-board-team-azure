521a59b31f5c92651b616267c44768a6997ac785
Romanek <89982697+RomanGumeniuk@users.noreply.github.com>
2024-01-12 10:58:13 +0100

    Jeszcze raz chakra

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import * as React from 'react';
+import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom';
 import App from './App.tsx';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ChakraProvider>
     <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
