231d8293b44213e04a04242dd0fbecf079473fcb
JakubWisla-WTT <jakub.wisla1@wundermanthompson.com>
2024-01-12 10:04:57 +0100

    mała poprawa obsługi bazy

--- my-app/src/AddNotes.jsx
+++ my-app/src/AddNotes.jsx
@@ -4,14 +4,16 @@ import axios from 'axios'; // biblioteka do wysyłania żądań HTTP
 function Notes() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [taskId, setTaskId] = useState(0);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:3000/notes', {
+      const response = await axios.post('http://localhost:3000/', {
         title: title,
-        content: content
+        content: content,
+        taskId: taskId
       });
 
       console.log(response.data);
@@ -30,6 +32,9 @@ function Notes() {
             <br /><br />
             <textarea id="textArea" name="textArea" placeholder="Content"
               value={content} onChange={e => setContent(e.target.value)}></textarea>
+            <br />
+            <input type="number" id="taskId" name="taskId" placeholder="Task ID" required 
+              value={taskId} onChange={e => setTaskId(e.target.value)} />
             <br />
             <input type="submit" id="submitButton" value="Submit" />
           </form>
