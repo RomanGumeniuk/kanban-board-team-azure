d385e779cbfecd49f8bced49e60692cbbd1f4eb0
Romanek <89982697+RomanGumeniuk@users.noreply.github.com>
2024-01-25 11:16:23 +0100

    Added AddtaskModal as new component

--- my-app/src/components/Column/Column.tsx
+++ my-app/src/components/Column/Column.tsx
@@ -7,32 +7,16 @@ import {
   IconButton,
   Stack,
   useColorModeValue,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalCloseButton,
-  FormControl,
-  FormLabel,
-  Input,
-  Button,
   useBreakpointValue,
   StackDirection,
-  VStack,
-  Radio,
-  RadioGroup,
-  useToast,
-  Textarea,
-  Flex,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { ColumnType } from "../../utils/enums";
 import Task from "../Task/Task";
 import { TaskModel } from "../../utils/models";
-import React from "react";
 import SimpleBar from "simplebar-react";
 import "simplebar/dist/simplebar.min.css";
+import AddTaskModal from "./AddTaskModal";
 
 const ColumnColorScheme: Record<ColumnType, string> = {
   TO_DO: "gray",
@@ -42,11 +26,8 @@ const ColumnColorScheme: Record<ColumnType, string> = {
 };
 
 function Column({ column }: { column: ColumnType }) {
-  const toast = useToast();
-  const [value, setValue] = React.useState("1");
   const [isOpen, setIsOpen] = useState(false);
   const onClose = () => setIsOpen(false);
-  const initialRef = React.useRef<HTMLInputElement | null>(null);
 
   const stackDirection = useBreakpointValue({
     base: "row",
@@ -55,25 +36,6 @@ function Column({ column }: { column: ColumnType }) {
 
   //Zmiana koloru scrolla
   const simpleBarColor = useColorModeValue("lightColor", "darkColor");
-
-  const handleAddTaskConfirm = () => {
-    toast({
-      title: "Task Added.",
-      description: "The task has been successfully created.",
-      status: "success",
-      duration: 5000,
-      isClosable: true,
-      position: "bottom",
-    });
-    onClose();
-  };
-
-  const handleKeyDown = (event: React.KeyboardEvent) => {
-    if (event.key === "Enter" && event.ctrlKey) {
-      handleAddTaskConfirm();
-    }
-  };
-
   const ColumnTasks = mockData.tasks
     .filter((task) => task.column === column)
     .map((task, index) => {
@@ -163,62 +125,7 @@ function Column({ column }: { column: ColumnType }) {
           </Stack>
         </SimpleBar>
       </Box>
-      <Modal
-        initialFocusRef={initialRef}
-        isOpen={isOpen}
-        onClose={onClose}
-        size="xl"
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <form onKeyDown={handleKeyDown}>
-            <ModalHeader>Add new task</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody pb={6}>
-              <FormControl>
-                <FormLabel>Title</FormLabel>
-                <Input placeholder="Title" ref={initialRef} />
-              </FormControl>
-
-              <FormControl mt={4}>
-                <FormLabel>Description</FormLabel>
-                <Textarea h={90} maxH={100} placeholder="Description" />
-              </FormControl>
-            </ModalBody>
-
-            <VStack alignItems="center">
-              <RadioGroup onChange={setValue} value={value}>
-                <Stack direction="row">
-                  <Radio value="1" colorScheme="gray">
-                    To do
-                  </Radio>
-                  <Radio value="2" colorScheme="yellow">
-                    In progress
-                  </Radio>
-                  <Radio value="3" colorScheme="blue">
-                    For review
-                  </Radio>
-                  <Radio value="4" colorScheme="green">
-                    Completed
-                  </Radio>
-                </Stack>
-              </RadioGroup>
-            </VStack>
-            <Flex justifyContent="center">
-              <Button
-                w={"40%"}
-                mt={4}
-                colorScheme="blue"
-                onClick={handleAddTaskConfirm}
-                variant={"solid"}
-                mb={0.5}
-              >
-                Save
-              </Button>
-            </Flex>
-          </form>
-        </ModalContent>
-      </Modal>
+      <AddTaskModal isOpen={isOpen} onClose={onClose} />
     </Box>
   );
 }
--- my-app/src/components/Task/TaskDrawer.tsx
+++ my-app/src/components/Task/TaskDrawer.tsx
@@ -132,36 +132,35 @@ function TaskDrawer({ isOpen, onClose, task }: TaskDrawerProps) {
 
                 <Heading size="md">Color</Heading>
                 <Flex>
-  <Box width="25%" p={2}>
-    <Circle
-      color="green"
-      selectedColor={selectedColor}
-      onSelect={handleColorSelect}
-    />
-  </Box>
-  <Box width="25%" p={2}>
-    <Circle
-      color="blue"
-      selectedColor={selectedColor}
-      onSelect={handleColorSelect}
-    />
-  </Box>
-  <Box width="25%" p={2}>
-    <Circle
-      color="red"
-      selectedColor={selectedColor}
-      onSelect={handleColorSelect}
-    />
-  </Box>
-  <Box width="25%" p={2}>
-    <Circle
-      color="gray"
-      selectedColor={selectedColor}
-      onSelect={handleColorSelect}
-    />
-  </Box>
-</Flex>
-
+                  <Box width="25%" p={2}>
+                    <Circle
+                      color="green"
+                      selectedColor={selectedColor}
+                      onSelect={handleColorSelect}
+                    />
+                  </Box>
+                  <Box width="25%" p={2}>
+                    <Circle
+                      color="blue"
+                      selectedColor={selectedColor}
+                      onSelect={handleColorSelect}
+                    />
+                  </Box>
+                  <Box width="25%" p={2}>
+                    <Circle
+                      color="red"
+                      selectedColor={selectedColor}
+                      onSelect={handleColorSelect}
+                    />
+                  </Box>
+                  <Box width="25%" p={2}>
+                    <Circle
+                      color="gray"
+                      selectedColor={selectedColor}
+                      onSelect={handleColorSelect}
+                    />
+                  </Box>
+                </Flex>
                 <Heading size="md">Image</Heading>
                 <Box>
                   <Input
